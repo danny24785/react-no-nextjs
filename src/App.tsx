171055d983584cdf8ed5196af19e1960890c34fc
dhalbesma <d.halbesma@blisdigital.com>
2025-12-18 11:11:33 +0100

    refactor: remove logos feat: add styling to menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,20 +6,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <ul>
+      <div className='flex'>
+        <ul className='flex gap-4'>
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
