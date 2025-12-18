1130d1abafb4bcb473013b80e3eea0303cdacbff
dhalbesma <d.halbesma@blisdigital.com>
2025-12-18 21:05:59 +0100

    feat: add disabled prop to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,10 @@
 type ButtonProps = {
   label: string;
   icon?: React.ReactNode;
+  disabled?: boolean;
   onClick?: () => void;
 };
 
-export default function Button({ label, icon, onClick }: ButtonProps) {
-  return <button onClick={onClick} className="flex gap-2 items-center py-2 px-4 rounded-md border-1 border-transparent bg-gray-700 hover:cursor-pointer">{icon && icon}{label}</button>;
+export default function Button({ label, icon, disabled, onClick }: ButtonProps) {
+  return <button onClick={onClick} disabled={disabled} className="flex gap-2 items-center py-2 px-4 rounded-md border-1 border-transparent bg-gray-700 hover:cursor-pointer">{icon && icon}{label}</button>;
 }
