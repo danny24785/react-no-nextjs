d6d896ce5eb03bd8b0a3058f738de8b5ccaf1e94
dhalbesma <d.halbesma@blisdigital.com>
2025-12-18 21:23:46 +0100

    refactor: open subnavigation on hover in stead of on click

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react';
 import MainNavigationItem from './MainNavigationItem';
 import { routes } from '../../routes';
 
 function MainNavigation() {
-  const [isAboutMenuOpen, setIsAboutMenuOpen] = useState(false);
-
   return (
     <div className='flex'>
       <ul className='flex gap-4'>
         <MainNavigationItem to={routes.home} label="Home" />
-        <MainNavigationItem onClick={() => setIsAboutMenuOpen(!isAboutMenuOpen)} label="About">
-          {isAboutMenuOpen && <ul className="absolute bg-gray-500">
+        <MainNavigationItem label="About">
+          <ul className="absolute bg-gray-500 hidden group-hover:flex flex-col">
             <MainNavigationItem to={routes.about} label="Sub Page 1" />
             <MainNavigationItem to={routes.subPage2} label="Sub Page 2" />
             <MainNavigationItem to={routes.subPage3} label="Sub Page 3" />
-          </ul>}
+          </ul>
         </MainNavigationItem>
         <MainNavigationItem to={routes.contact} label="Contact" />
       </ul>
