c53d2cad59da782dc74a5955d8120f8e8368c611
dhalbesma <d.halbesma@blisdigital.com>
2025-12-19 08:14:01 +0100

    feat: add typing to useQuery

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,8 +1,9 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { fetchPosts } from '../api/posts'
+import { type Post } from '../types/todo';
 
 export function usePosts(page: number, pageLimit: number) {
-  return useQuery({
+  return useQuery<Post[]>({
     queryKey: ['posts', page, pageLimit],
     queryFn: () => fetchPosts(page, pageLimit),
     placeholderData: keepPreviousData,
